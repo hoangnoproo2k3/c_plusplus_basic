91f0c5392de0cddbfb66c808f5ca2e9a724874fe
hoangnoproo2k3 <108531781+hoangnoproo2k3@users.noreply.github.com>
2022-08-10 09:32:58 +0700

    last

--- BaiTapThucHanh/bai-42-1 (1).cpp
+++ BaiTapThucHanh/bai-42-1 (1).cpp
@@ -1,197 +1,201 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-
-struct PHIM {
-    char tenphim[50],hoten[50],hangsx[20],theloai[20];
-    int namsx;
+struct Phim{
+	string tenphim,daodien,hangsx,theloai;
+	int namsx;
 };
-
-PHIM nhap() {
-    PHIM p;
-    cin.ignore();
-    cout<<"\nMoi nhap ten phim: ";
-    cin.getline(p.tenphim,50);
-    cout<<"Moi nhap ho ten dao dien: ";
-    cin.getline(p.hoten,50);
-    cout<<"Moi nhap hang sx: ";
-    cin.getline(p.hangsx,20);
-    cout<<"Moi nhap the loai: ";
-    cin.getline(p.theloai,20);
-    cout<<"Moi nhap nam sx: ";
-    cin>>p.namsx;
-    return p;
+void tentieude(){
+	cout<<setw(10)<<left<<"STT"
+	<<setw(15)<<left<<"TEN PHIM"
+	<<setw(15)<<left<<"TEN DAO DIEN"
+	<<setw(15)<<left<<"HANG SX"
+	<<setw(15)<<left<<"NAM SX"
+	<<setw(15)<<left<<"THE LOAI";
+}
+void nhap(Phim &n){
+	cout<<"nhap ten phim: ";
+	getline(cin,n.tenphim);
+	cout<<"nhap ten dao dien: ";
+	getline(cin,n.daodien);
+	cout<<"nhap hang san xuat: ";
+	getline(cin,n.hangsx);
+	cout<<"nhap nam san xuat: ";
+	cin>>n.namsx;
+	cin.ignore();
+	cout<<"nhap the loai phim: ";
+	getline(cin,n.theloai);
+}
+void xuat(Phim n){
+	cout<<setw(15)<<left<<n.tenphim;
+	cout<<setw(15)<<left<<n.daodien;
+	cout<<setw(15)<<left<<n.hangsx;
+	cout<<setw(15)<<left<<n.namsx;
+	cout<<setw(15)<<left<<n.theloai;
+}
+void nhapp(Phim n[],int &b){
+	cout<<"nhap so luong phim: ";
+	cin>>b;
+	cin.ignore();
+	for(int i=0;i<b;i++){
+		nhap(n[i]);
+	}
+}
+void xuatt(Phim n[], int b){
+	tentieude();
+	for(int i=0;i<b;i++){
+		cout<<"\n"<<setw(10)<<i+1;
+		xuat(n[i]);
+	}
+}
+void ghi(Phim n[], int &b){
+	fstream f;
+	f.open("PHIM.DAT", ios::out | ios::binary);
+	f.write((char*) &b, sizeof(int));
+	for(int i=0;i<b;i++){
+		f.write((char*) &n[i], sizeof(Phim));
+	}
+	cout<<"ghi file thanh cong";
+	f.close();
+}
+void xuatfile(Phim n[], int &b){
+	fstream f;
+	f.open("PHIM.DAT", ios::in | ios::binary);
+	f.write((char*) &b, sizeof(int));
+	for(int i=0;i<b;i++){
+		f.read((char*) &n[i], sizeof(Phim));
+	}
+	f.close();
+}
+void namsx(Phim n[], int b){
+	int nam;
+	cout<<"nhap nam san xuat muon tim: ";
+	cin>>nam;
+	cin.ignore();
+	tentieude();
+	for(int i=0;i<b;i++){
+		if(n[i].namsx==nam){
+			cout<<"\n"<<setw(10)<<i+1;
+			xuat(n[i]);
+		}
+	}
+}
+void sapxep(Phim n[],int &b){
+	fstream f;
+	f.open("PHIM.DAT", ios::out | ios::binary);
+	f.write((char*) &b, sizeof(int));
+	for(int i=0;i<b;i++){
+		for(int j=0;j<b;j++){
+			if(n[i].namsx<n[j].namsx){
+				swap(n[i],n[j]);
+			}
+		}
+	}
+	xuatt(n,b);
+	for(int i=0;i<b;i++){
+		f.write((char*) &n[i], sizeof(Phim));
+	}
+	f.close();
 }
 
-void hien(PHIM p, int i) {
-    cout<<setw(5)<<p.tenphim<<setw(25)<<p.hoten<<setw(20)<<p.hangsx<<setw(15)<<p.theloai<<setw(10)<<p.namsx<<endl;
+void giaiphong(Phim n[], int b){
+	int dem=0;
+	tentieude();
+	for(int i=0;i<b;i++){
+		if(n[i].hangsx== "giai phong"){
+			dem++;
+			cout<<"\n"<<setw(10)<<i+1;
+			xuat(n[i]);
+		}
+	}
+	if(dem==0){
+		cout<<"khong co bo phim nao do hang giai phong san xuat! "<<endl;
+	}
+	else{
+		cout<<"co"<<dem<<"bo phim do hang giai phong san xuat ";
+	}
 }
+int main(){
+	fstream f;
+	int b,chon;
+	Phim n[100];
+	bool kiemtra=false;
+	do{
+		cout<<"\n|------------------------MENU----------------------------|"
+		<<"\n| 1.Nhap so phim "
+		<<"\n| 2.luu vao tep PHIM.DAT "
+		<<"\n| 3.doc file "
+		<<"\n| 4.hien thi phim ra man hinh  "
+		<<"\n| 5.Nhap nam san xuat tim phim	"
+		<<"\n| 6.sap xep phim theo nam giam dan, ghi vao SAPXEPNAM.DAT"
+		<<"\n| 7.so bo phim do hang phim giai phong san xuat 		  "
+		<<"\n| 8.Thoat chuong trinh ";
+		cout<<"\n moi ban chon: ";
+		cin>>chon;
+		cin.ignore();
+		switch(chon){
+			case 1:
+				nhapp(n,b);
+				kiemtra=true;
+				break;
 
-void nhapds(PHIM ds[], int &n) {
-    cout<<"Moi nhap so phim: ";
-    cin>>n;
-    for(int i=0;i<n;i++) {
-        ds[i]=nhap();
-    }
-}
+			case 2:
+				if(kiemtra){
+				ghi(n,b);
+				break;}
+				else{
+					cout<<"moi nhap lai";
+					break;
+				}
 
-void hiends(PHIM ds[], int n) {
-    cout<<"\n\n\t\tDANH SACH PHIM"<<endl;
-    cout<<setw(5)<<"Ten phim"<<setw(25)<<"Dao dien"<<setw(20)<<"Hang sx"<<setw(15)<<"The loai"<<setw(10)<<"Nam sx"<<endl;
-    for(int i=0;i<n;i++) {
-        hien(ds[i],i);
-    }
-}
+			case 3:
+				if(kiemtra){
+				xuatfile(n,b);
+				break;}
+				else{
+					cout<<"moi nhap lai";
+					break;
+				}
 
-void ghifile(PHIM ds[] ,int n) {
-    fstream f;
-    f.open("PHIM.dat",ios::out|ios::binary);
-    for(int i=0; i<n; i++)
-        f.write((char *) &ds[i],sizeof(PHIM));
+			case 4:
+				if(kiemtra){
+				xuatt(n,b);
+				break;}
+				else{
+					cout<<"moi nhap lai";
+					break;
+				}
 
-    if(!f.good()) {
-        cout<<"\n\n\t\t Ghi file that bai!";
-    }
-    else {
-        cout<<"\n\n\t\t Ghi file thanh cong!"<<endl;
-    }
-    f.close();
-}
+			case 5:
+				if(kiemtra){
+				namsx(n,b);
+				break;}
+				else{
+					cout<<"moi nhap lai";
+					break;
+				}
 
-void docfile(PHIM ds[] ,int &n) {
-    fstream f;
-    f.open("PHIM.dat", ios::in|ios::binary);
-    int i=0;
-    while(!f.eof()) //khi chua ket thuc file thi tiep tuc doc
-    {
-        f.read((char *) &ds[i],sizeof(PHIM));
-        i++;
-    }
-    n=i-1;
-    f.close();
-    cout<<"\n\n\t\t Doc file thanh cong!"<<endl;
+			case 6:
+				if(kiemtra){
+				sapxep(n,b);
+				break;}
+				else{
+					cout<<"moi nhap lai";
+					break;
+				}
 
-}
+			case 7:
+				if(kiemtra){
+				giaiphong(n,b);
+				break;}
+				else{
+					cout<<"moi nhap lai";
+					break;
+				}
+			default:{
+			cout<<"nhap lai!!! ";
+			break;}
+				}
+	}
+	while(chon!=8);
+	}
 
-void namsx(PHIM ds[], int &n) {
-    int k;
-    cout<<"Moi nhap nam can tim: ";
-    cin>>k;
-    cout<<setw(5)<<"Ten phim"<<setw(25)<<"Dao dien"<<setw(20)<<"Hang sx"<<setw(15)<<"The loai"<<setw(10)<<"Nam sx"<<endl;
-    for(int i=0; i<n; i++) {
-        if(ds[i].namsx==k) {
-            hien(ds[i],i);
-        }
-    }
-}
-
-void sapxep(PHIM ds[], int &n) {
-    PHIM tg;
-    for(int i=0; i<n-1; i++) {
-        for(int j=i+1; j<n; j++) {
-            if(ds[i].namsx<ds[j].namsx) {
-                tg=ds[i];
-                ds[i]=ds[j];
-                ds[j]=tg;
-            }
-        }
-    }
-    cout<<"\n\n\t\tDANH SACH PHIM SAU KHI SAP XEP"<<endl;
-    cout<<setw(5)<<"Ten phim"<<setw(25)<<"Dao dien"<<setw(20)<<"Hang sx"<<setw(15)<<"The loai"<<setw(10)<<"Nam sx"<<endl;
-     for(int i=0; i<n; i++) {
-            hien(ds[i],i);
-    }
-}
-
-void ghifileds(PHIM ds[] ,int n) {
-    fstream f;
-    f.open("SAPTHEONAM.dat",ios::out|ios::binary);
-    for(int i=0; i<n; i++)
-        f.write((char *) &ds[i],sizeof(PHIM));
-
-    if(!f.good()) {
-        cout<<"\n\n\t\t Ghi file that bai!";
-    }
-    else {
-        cout<<"\n\n\t\t Ghi file thanh cong!"<<endl;
-    }
-    f.close();
-}
-
-void docfileds(PHIM ds[] ,int &n) {
-    fstream f;
-    f.open("SAPTHEONAM.dat", ios::in|ios::binary);
-    int i=0;
-    while(!f.eof()) //khi chua ket thuc file thi tiep tuc doc
-    {
-        f.read((char *) &ds[i],sizeof(PHIM));
-        i++;
-    }
-    n=i-1;
-    f.close();
-    cout<<"\n\n\t\t Doc file thanh cong!"<<endl;
-    hiends(ds,n);
-}
-
-void dem(PHIM ds[], int &n) {
-    int dem=0;
-    for(int i=0; i<n; i++) {
-        if(strcmp(ds[i].hangsx,"Giai phong")==0) {
-            dem++;
-        }
-    }
-    cout<<"So phim do hang Giai phong thuc hien la: "<<dem<<endl;
-}
-
-
-int main() {
-    PHIM ds[100];
-    int n, chon;
-    do {
-
-        cout << "\n|------------------------ MENU -------------------------|"
-             << "\n| 1. Nhap danh sach phim                                |"
-             << "\n| 2. Ghi du lieu phim vao PHIM.dat                      |"
-             << "\n| 3. Doc file PHIM.dat                                  |"
-             << "\n| 4. Tim phim theo nam san xuat                         |"
-             << "\n| 5. Sap xep phim giam dan theo nam va ghi vao file     |"
-             << "\n|    SAPTHEONAM.dat                                     |"
-             << "\n| 6. Thong ke phim do hang Giai phong thuc hien         |"
-             << "\n| 0. Thoat...                                           |"
-             << "\n|-------------------------------------------------------|";
-        cout<<"\nBan chon: ";
-        cin>>chon;
-        switch(chon) {
-        case 1:
-            nhapds(ds, n);
-            break;
-        case 2:
-            ghifile(ds, n);
-            break;
-        case 3:
-            docfile(ds, n);
-            hiends(ds, n);
-            break;
-        case 4:
-            namsx(ds, n);
-            break;
-        case 5:
-            sapxep(ds, n);
-            ghifileds(ds, n);
-            break;
-        case 6:
-            dem(ds, n);
-            break;
-        case 0:
-            cout << "Cam on ban da dung dich vu.\n";
-            return 0;
-            break;
-        default:
-            cout << "Option ban chon khong co trong menu...\n";
-            break;
-        }
-        system("pause");
-        system("cls");
-    }
-    while(chon != 0);
-    return 1;
-}
